5e8089b327056e0d297e27cffab71cd616a4b545
Herdran <jakub4426@gmail.com>
2022-11-05 15:31:29 +0100

    New algo initial implementation

--- back/algo.py
+++ back/algo.py
@@ -1,42 +1,31 @@
+from back.constans import reverse_parser
 from back.data_loader import get_dataframe_from_json
-import pandas as pd
 
 
-def calculate_weight(row, parameter, values):
-    return values[str(row[parameter])]
+def calculate_result(row, problem, satisfaction_threshold):
+    return 1 if satisfaction_threshold > row[problem] else 0
 
 
-def calculate_result(row, parameter, satisfactionThreshold):
-    return row["weight"] * (1 if satisfactionThreshold > row[parameter] else 0)
-    # return row["weight"] * max(satisfactionThreshold - row[parameter], 0)
+def calculate_score(df, params, problems, satisfaction_threshold):
+    query_string = ""
+
+    for param in params:
+        query_string += reverse_parser[param] + "=='" + param + "'and "
+
+    query_string = query_string[:-4]
+    df_queried = df.query(query_string)
+
+    score = 0
+
+    for problem in problems:
+        score += sum(df_queried.apply(lambda row: calculate_result(row, problem, satisfaction_threshold), axis=1))
+
+    return score
 
 
-def algo(params, satisfactionThreshold):
-    df = get_dataframe_from_json()
-    X = df.copy()
-    X["weight"] = 0
+params = ["Male", "Business"]
+problems = ["wifiService", "onboardService"]
+satisfaction_threshold = 3
 
-    # print(XCopy["weight"])
-
-    example_input_weights = params[0]
-    example_input_problems = params[1]
-
-    for key in example_input_weights.keys():
-        X["weight"] += X.apply(lambda row: calculate_weight(row, key, example_input_weights[key]), axis=1)
-
-    # print(XCopy["weight"])
-    # print(sum(XCopy["weight"]))
-
-    res = pd.DataFrame()
-    # print(res)
-    for problem in example_input_problems:
-        res[problem] = X.apply(lambda row: calculate_result(row, problem, satisfactionThreshold), axis=1)
-
-    # print(res)
-    # print(sum(res["wifiService"]))
-
-
-params = [{"gender": {"Female": 2, "Male": 1}, "travelClass": {"Eco Plus": 0, "Eco": 1, "Business": 4}},
-          ["wifiService"]]
-
-algo(params, 3)
+df = get_dataframe_from_json()
+calculate_score(df, params, problems, satisfaction_threshold)
