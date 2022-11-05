6dd42b986013bb60149723ffac2336655e868ad0
Pawel Gorgolewski <pgorgolewski01@gmail.com>
2022-11-05 12:30:55 +0100

    create constants file

--- back/app.py
+++ back/app.py
@@ -1,52 +1,7 @@
 from flask import Flask, jsonify
+from constans import reviews, info
 
 app = Flask(__name__)
-
-reviews = {
-    "wifiService": "Wifi Service",
-    "timeConvenient": "Time Convenient",
-    "bookingEase": "Booking Ease",
-    "gateLocation": "Gate Location",
-    "food": "Food",
-    "boarding": "Boarding",
-    "seatComfort": "Seat Comfort",
-    "entertainment": "Entertainment",
-    "onboardService": "Onboard Service",
-    "legRoomService": "Leg Room Service",
-    "baggageHandling": "Baggage Handling",
-    "checkinService": "CheckinService",
-    "inflightService": "Inflight Service",
-    "cleanliness": "Cleanliness",
-    "departureDelayInMinutes": "Departure Delay In Minutes",
-    "arrivalDelayInMinutes": "Arrival Delay In Minutes",
-}
-
-info = {
-    "travelType": {
-        "name": "Travel Type",
-        "values": ["Personal Travel", "Business Travel"]
-    },
-    "travelClass": {
-        "name": "Travel Class",
-        "values": ["Business", "Eco", "Eco Plus"]
-    },
-    "travelDistance": {
-        "name": "Travel Distance",
-        "values": ["Short", "Middle", "Long"]
-    },
-    "gender": {
-        "name": "Name",
-        "values": ["Female", "Male"]
-    },
-    "type": {
-        "name": "Passenger Type",
-        "values": ["Loyal customer, Disloyal Customer"]
-    },
-    "age": {
-        "name": "Age",
-        "values": ["Child", "Young Adult", "Mid Adult", "Elder"]
-    }
-}
 
 
 @app.route('/')
@@ -60,6 +15,7 @@ def get_types():
     jdata['review'] = list(reviews.values())
     jdata['info'] = [dict(name=col_info['name'], values=col_info['values']) for col_info in info.values()]
     return jsonify(jdata)
+
 
 if __name__ == '__main__':
     app.run()
